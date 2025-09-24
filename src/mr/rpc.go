81ccec02377332db26e395f1a931db0318809a67
linnaid <jiaguoyangyang@qq.com>
2025-09-24 20:45:06 +0800

    Lab-1

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -24,6 +24,40 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+// Worker 请求任务
+type TaskRequestArgs struct {
+	WorkerID int;
+}
+
+type TaskRequestReply struct {
+	TaskType string  // 任务类型
+	TaskID int   // 任务编号
+	FileName string  // map任务的输入文件名
+	NReduce int   // Reduce数量
+	NMap int 
+}
+
+
+// Worker 完成任务后上报结果
+type TaskCompleteArgs struct {
+	WorkerID int   // 完成任务的Worker
+	TaskType string    // 任务类型
+	TaskID int   // 任务编号
+}
+
+type TaskCompleteReply struct {
+	Ack bool   // 是否继续请求新任务
+}
+
+
+
+type RequestArgs struct {
+	WorkerID int;
+}
+
+type RequestReply struct {
+	WorkerID int;
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
